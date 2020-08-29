1e1daf706de7c5f05df9fb358bef9b3cc96c7571
Hayley Hughes <hayley@foxes.systems>
2020-08-29 12:56:36 +1000

    Add the index route

--- logistik/views/index.py
+++ logistik/views/index.py
@@ -1,8 +1,16 @@
 from logistik import app
 from flask import render_template
+from . import login_required
 
 
-@app.route("/hello/<name>")
-@app.route("/hello")
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html')
+
+
+@app.route('/hello/<name>')
+@app.route('/hello')
+@login_required
 def hello(name):
-    return render_template("index.html", name=name)
+    return render_template('hello.html', name=name)
