95b171c4aa1e434e49206906cbc034ca98976ea3
pinnymc <63958584+pinnymc@users.noreply.github.com>
2020-09-10 16:51:05 +1000

    Chnages made

--- logistik/models/user.py
+++ logistik/models/user.py
@@ -8,7 +8,7 @@ class User(db.Model):
     last_name = db.Column(db.String(), nullable=False)
     first_name = db.Column(db.String(), nullable=False)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
-    status_id = db.Column(db.Integer, db.FOreinKey('status.id'), nullable=False)
+    status_id = db.Column(db.Integer, db.ForeinKey('status.id'), nullable=False)
 
     def __repr__(self):
         return f'User {self.name}'
@@ -27,15 +27,15 @@ class Assets(db.Model):
     location = db.Column(db.Integer, db.ForeignKey(
         'location.id'), nullable=False)
     asset_type = db.Column(db.Integer, db.ForeignKey(
-        'asset_type.id'), nullable=False)
+        'assettype.id'), nullable=False)
     status = db.Column(db.Integer, db.ForeignKey('status.id'), nullable=False)
 
 
-class Asset_type(db.Model):
+class AssetType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    type_of_asset = db.Column(db.String(), nullable=False)
-    assets = db.relationship('Asset', backref='asset_type', lazy=True)
+    type = db.Column(db.String(), nullable=False)
+    assets = db.relationship('Asset', backref='assettype', lazy=True)
 
 
 class Location(db.Model):
@@ -47,7 +47,7 @@ class Location(db.Model):
 
 class Status(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    change_time = db.Column(db.String(), nullable=False)
+    time = db.Column(db.String(), nullable=False)
     description = db.Column(db.String(), nullable=False)
     note = db.Column(db.String(), nullable=True)
     assets = db.relationship('Asset', backref='status', lazy=True)
