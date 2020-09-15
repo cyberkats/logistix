5d09c8ef06fbfd923edd23a16af4643a0a451bb2
Hayley Hughes <hayley@foxes.systems>
2020-09-15 17:04:49 +1000

    Trim model fat

--- logistik/models.py
+++ logistik/models.py
@@ -5,34 +5,21 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
-    last_name = db.Column(db.String(), nullable=False)
-    first_name = db.Column(db.String(), nullable=False)
-    role_id = db.Column(db.Integer, db.ForeignKey('role.id'), nullable=False)
-    status_id = db.Column(db.Integer, db.ForeignKey('status.id'), nullable=False)
 
     def __repr__(self):
         return f'User {self.name}'
-
-
-class Role(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), unique=True, nullable=False)
-    description = db.Column(db.String(), nullable=False)
-    users = db.relationship('User', backref='role', lazy=True)
 
 
 class AssetType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     type = db.Column(db.String(), nullable=False)
-    assets = db.relationship('Asset', backref='asset_type', lazy=True)
 
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     latitiude = db.Column(db.Float(), nullable=False)
     longitude = db.Column(db.Float(), nullable=False)
-    assets = db.relationship('Asset', backref='location', lazy=True)
 
 
 class Status(db.Model):
@@ -40,15 +27,13 @@ class Status(db.Model):
     time = db.Column(db.String(), nullable=False)
     description = db.Column(db.String(), nullable=False)
     note = db.Column(db.String(), nullable=True)
-    assets = db.relationship('Asset', backref='status', lazy=True)
-    users = db.relationship('User', backref='status', lazy=True)
 
 
 class Asset(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    status = db.Column(db.Integer, db.ForeignKey('status.id'), nullable=False)
     location = db.Column(db.Integer, db.ForeignKey(
         'location.id'), nullable=True)
     asset_type = db.Column(db.Integer, db.ForeignKey(
         'asset_type.id'), nullable=True)
-    status = db.Column(db.Integer, db.ForeignKey('status.id'), nullable=False)
