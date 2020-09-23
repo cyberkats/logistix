6fc4eaff1f8717f611bb769b0a40e18e9b4321df
Hayley Hughes <hayley@foxes.systems>
2020-09-23 16:26:33 +1000

    Add asset map

--- logistik/static/map.js
+++ logistik/static/map.js
@@ -1,17 +1,1 @@
-"use strict";
 
-let map;
-
-function initMap() {
-  var melbourne = {lat: -37.813611, lng: 144.963056};
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: melbourne,
-    zoom: 8
-  });
-  var marker = new google.maps.Marker({
-    position: {
-      lat: -38.15,
-      lng: 144.35
-    }, map: map
-  });
-}
