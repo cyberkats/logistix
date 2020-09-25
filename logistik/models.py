98ead1cf6af6f2dcc65299226f7643fc7d62ea52
Hayley Hughes <hayley@foxes.systems>
2020-09-25 13:41:31 +1000

    Make models more consistant

--- logistik/models.py
+++ logistik/models.py
@@ -6,9 +6,6 @@ class User(db.Model):
     name = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
 
-    def __repr__(self):
-        return f'User {self.name}'
-
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
